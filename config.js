67d5d3c7c1450a9a26ec82f26bb318df6db5d9a2
Evyphan <86779305+Evyphan@users.noreply.github.com>
2021-07-01 18:20:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    introTitle: 'Hi cưng!',
+    introDesc: `Trả lời câu hỏi tiếp theo nè`,
+    btnIntro: 'ahihi',
+    title: 'Thư có xinh không ạ? 😙',
+    desc: 'Xinh lắm chứ còn gì nữa :)) ',
+    btnYes: 'Xinhh.. tuyêt vời ông mặt trời <33',
+    btnNo: 'Không xinh lắm :(((',
+    question:'Sáng suốt thế! Mình xinh như nào nè?? <3',
+    btnReply: 'Suy nghĩ cho kĩ trước khi gửi nè <3',
+    reply: 'Cái nào cũng xinh, Thư xinh đẹp  <33333333',
+    mess: 'Cảm ơn cảm ơn.. 🥰. Ngại quớ 😘😘',
+    messDesc: 'Sang đây hun miếng nào.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://chat.zalo.me/
 }
